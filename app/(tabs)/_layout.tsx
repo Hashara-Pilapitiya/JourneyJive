0eecf27b4cd2fae0decfe31955466cf525e6bf65
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-24 13:48:48 +0530

    Optimize tab icons in _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,1 +1,24 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons'
 
+export default function Layout() {
+  return (
+
+    <Tabs>
+
+      <Tabs.Screen name="index" options={{tabBarIcon: ({color}) => (<Ionicons name='compass' size={28} color={color} />)}} />
+
+      <Tabs.Screen name="category"  options={{tabBarIcon: ({color}) => (<MaterialIcons name="space-dashboard" size={28} color={color} /> )}} />
+
+      <Tabs.Screen name="search" options={{tabBarIcon: ({color}) => (<Ionicons name='search' size={28} color={color} />)}} />
+
+      <Tabs.Screen name="bookmarks" options={{tabBarIcon: ({color}) => (<Ionicons name='bookmark' size={28} color={color} />)}} />
+
+      <Tabs.Screen name="profile" options={{tabBarIcon: ({color}) => (<FontAwesome name='user' size={28} color={color} />)}} />
+
+    </Tabs>
+    
+  )
+}
