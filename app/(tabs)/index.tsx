9bc82c4d32afe1cedc99043e7b415967f122e79e
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-24 14:44:56 +0530

    Add header with profile image and notification icon to Page component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
 const Page = () => {
   return (
-    <View style={styles.container}>
-      <Text>Page</Text>
-    </View>
+    <Stack.Screen options={{ headerTransparent: true, headerTitle: '', headerLeft: () => (
+      <TouchableOpacity onPress={() => {}} style= {{marginLeft: 20}}>
+        <Image source={{uri: 'https://static.vecteezy.com/system/resources/previews/026/829/465/non_2x/beautiful-girl-with-autumn-leaves-photo.jpg'}} style={{width: 50, height: 50, borderRadius: 50}} />
+      </TouchableOpacity>
+   ),
+   headerRight: () => (
+    <TouchableOpacity onPress={() => {}} style={{marginRight: 20, marginTop: -12, backgroundColor: Colors.secondory, padding: 10, borderRadius: 50, shadowColor: '#E0E0E0', shadowOffset:  { width:2, height:4 }, shadowOpacity: 0.2, shadowRadius: 3}}>
+      <Ionicons name='notifications' size={20} color='#000000' />
+    </TouchableOpacity>
+   ) }}  />
   )
 }
 
